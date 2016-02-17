6ade6f16ae20b0749aca157693e61b4b487c1f6c
Alec <aleczhang@163.com>
2016-02-17 17:29:28 +0800

    helloworld

---
+++ app/main.js
@@ -0,0 +1,6 @@
+'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './Hello';
+
+ReactDOM.render(<Hello />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var webpack = require('webpack');
+
+
+module.exports = {
+    entry: [
+      'webpack/hot/only-dev-server',
+      'webpack-dev-server/client?http://localhost:8080',
+       './app/main.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    module: {
+    loaders: [  
+    { // 参照的这个日志（http://jamesknelson.com/unlocking-decorators-and-other-es7-features-with-webpack-and-babel/）里的评论
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /node_modules/
+      },
+    {
+        test: /node_modules\/react-colorpickr\/.*\.js/,
+        loader: "babel-loader"
+      }]
+  }
+};
+
